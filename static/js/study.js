b15c35af6efad43d06edb87c493b4bf95c76b40c
neotechz <marc.ayalde@gmail.com>
2024-12-19 18:38:47 +0800

    Add next card functionality in study.html

--- static/js/study.js
+++ static/js/study.js
@@ -1,16 +1,27 @@
+const cards = document.getElementsByClassName("card hidden");
+
 const board = document.getElementById("board");
 const board_question = board.querySelector(".question");
 const board_answer = board.querySelector(".answer");
-const board_reveal = board.querySelector(".reveal");
+
+let index = 0;
 
 document.addEventListener("DOMContentLoaded", function(){
-    const cards = document.getElementsByClassName("card hidden");
-    let index = 0;
-    
     board_question.value = cards[index].querySelector(".front").value;
     board_answer.value = cards[index].querySelector(".back").value;
 });
 
+const board_reveal = board.querySelector(".reveal");
 board_reveal.addEventListener("click", function(){
     board_answer.type = "text";
-})
+});
+
+const board_next = board.querySelector(".next");
+board_next.addEventListener("click", function(){
+    index += 1;
+    index %= cards.length;
+
+    board_question.value = cards[index].querySelector(".front").value;
+    board_answer.value = cards[index].querySelector(".back").value;
+    board_answer.type = "hidden";
+});
