97f889fb2dda316a3ac14aa6f7ca95a3ca5a4c8b
neotechz <marc.ayalde@gmail.com>
2024-12-19 13:57:30 +0800

    Move view deck functionality from index.html to index.js for flexibility

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,12 @@
+const decks = document.getElementsByClassName("deck");
+for(let deck of decks){
+    let deck_view = deck.querySelector(".view");
+    deck_view.addEventListener("click", function(){
+        deck.action = `/deck/${deck.id}`;
+        deck.submit()
+    });
+}
+
 const new_deck = document.getElementById("new_deck");
 new_deck.addEventListener("click", async function(event){
     const deck = await create_new_deck();
