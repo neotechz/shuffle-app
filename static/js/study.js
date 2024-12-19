a251b435f9f4873d4d8e1c300d17969f0c6a57e7
neotechz <marc.ayalde@gmail.com>
2024-12-19 15:22:20 +0800

    Add reveal functionality in study.html

--- static/js/study.js
+++ static/js/study.js
@@ -1,9 +1,16 @@
+const board = document.getElementById("board");
+const board_question = board.querySelector(".question");
+const board_answer = board.querySelector(".answer");
+const board_reveal = board.querySelector(".reveal");
+
 document.addEventListener("DOMContentLoaded", function(){
     const cards = document.getElementsByClassName("card hidden");
     let index = Math.floor(Math.random() * cards.length);
-
-    const board = document.getElementById("board");
-    const board_question = board.querySelector(".question");
     
     board_question.value = cards[index].querySelector(".front").value;
+    board_answer.value = cards[index].querySelector(".back").value;
 });
+
+board_reveal.addEventListener("click", function(){
+    board_answer.type = "text";
+})
