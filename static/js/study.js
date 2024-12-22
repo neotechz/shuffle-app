8241a70798c03698a926381e1224049dfb14eddd
neotechz <marc.ayalde@gmail.com>
2024-12-22 12:47:23 +0800

    Modify next button in study.html to be disabled at last element

--- static/js/study.js
+++ static/js/study.js
@@ -3,12 +3,17 @@ const cards = document.getElementsByClassName("card hidden");
 const board = document.getElementById("board");
 const board_question = board.querySelector(".question");
 const board_answer = board.querySelector(".answer");
+const board_next = board.querySelector(".next");
 
 let index = 0;
 
 document.addEventListener("DOMContentLoaded", function(){
     board_question.value = cards[index].querySelector(".front").value;
     board_answer.value = cards[index].querySelector(".back").value;
+
+    if (index >= cards.length - 1){
+        board_next.disabled = true;
+    }
 });
 
 const board_reveal = board.querySelector(".reveal");
@@ -16,10 +21,13 @@ board_reveal.addEventListener("click", function(){
     board_answer.type = "text";
 });
 
-const board_next = board.querySelector(".next");
+
 board_next.addEventListener("click", function(){
     index += 1;
-    index %= cards.length;
+
+    if (index >= cards.length - 1){
+        board_next.disabled = true;
+    }
 
     board_question.value = cards[index].querySelector(".front").value;
     board_answer.value = cards[index].querySelector(".back").value;
